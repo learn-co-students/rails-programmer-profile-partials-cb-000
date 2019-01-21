f3ee6fc293e1794945cd552a999c7ad1d6b4aa44
Fer Hernandez <fh.1111@gmail.com>
2019-01-21 18:05:26 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407131620) do
+ActiveRecord::Schema.define(version: 2015_04_07_131620) do
 
   create_table "programmers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "home_country"
-    t.string   "quote"
-    t.string   "claim_to_fame"
-    t.string   "image"
-    t.string   "wikipedia_page"
-    t.date     "birth_date"
-    t.date     "death_date"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string "name"
+    t.string "home_country"
+    t.string "quote"
+    t.string "claim_to_fame"
+    t.string "image"
+    t.string "wikipedia_page"
+    t.date "birth_date"
+    t.date "death_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
